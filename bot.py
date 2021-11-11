1e872a0c00d18399b8acb866ce36cc4d24b5afc1
uzapolsky <uzapolsky@gmail.com>
2021-11-11 13:23:25 +0300

    setup dialogflow

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from google.cloud import dialogflow
 from dotenv import load_dotenv
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
@@ -12,6 +13,21 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def detect_intent_texts(project_id, session_id, text, language_code):
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return(response.query_result.fulfillment_text)
+
+
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     update.message.reply_markdown_v2(
@@ -21,7 +37,14 @@ def start(update: Update, context: CallbackContext) -> None:
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    update.message.reply_text(
+        detect_intent_texts(
+            os.getenv('PROJECT_ID'),
+            update.message.from_user.id,
+            update.message.text,
+            'ru-RU')
+    )
+
 
 
 def main() -> None:
